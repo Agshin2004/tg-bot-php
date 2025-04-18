24b0e4181af924f7e918382165311ee0b3393a08
Agshin <nadirov.main@gmail.com>
2025-04-18 23:17:23 +0400

    init

---
+++ src/main.php
@@ -0,0 +1,33 @@
+<?php
+$parentDir = dirname(__FILE__, 2);
+// print_r($parentDir);
+// return;
+// require_once dir('/vendor/autoload.php');
+require_once $parentDir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use TelegramBot\Api\BotApi;
+$envPath = realpath(__DIR__ . '/../');
+if (!$envPath) {
+    die('.env file not found');    
+}
+// Load env variables
+(Dotenv\Dotenv::createImmutable($envPath))->load(); // Create a new immutable dotenv instance with default repository
+
+$token = $_ENV['TOKEN'];
+$adminId = $_ENV['ADMIN_ID'];
+
+$bot = new BotApi($token);
+
+try {
+    $bot->on(function (\TelegramBot\Api\Types\Update $update) use ($bot) {
+        $message = $update->getMessage();
+        $id = $message->getChat()->getId();
+        $bot->sendMessage($id, $message->getText());
+    }, function () {
+        return true;
+    });
+
+    $bot->run();
+} catch (\Exception $e) {
+    throw new \Exception($e->getMessage());
+}
